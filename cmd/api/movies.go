39cc51cf26f688d7bdf5a426a7ff50a472f96f67
Travis Hopkins <trh.foss@protonmail.com>
2025-05-13 08:05:05 -0400

    fix: added filter validations to listMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -98,7 +98,7 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		Title   *string       `json:"title"`
 		Year    *int32        `json:"year"`
 		Runtime *data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Genres  []string      `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -107,21 +107,21 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-  if input.Title != nil {
-    movie.Title = *input.Title
-  }
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
 
-  if input.Year != nil {
-    movie.Year = *input.Year
-  }
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
 
-  if input.Runtime != nil {
-    movie.Runtime = *input.Runtime
-  }
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
 
-  if input.Genres != nil {
-    movie.Genres = input.Genres
-  }
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
@@ -132,10 +132,10 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-    if errors.Is(err, data.ErrEditConflict) {
-      app.editConflictResponse(w, r)
-      return
-    }
+		if errors.Is(err, data.ErrEditConflict) {
+			app.editConflictResponse(w, r)
+			return
+		}
 		app.serverErrorResponse(w, r, err)
 		return
 	}
@@ -147,50 +147,57 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
-  id, err := app.readIdParam(r)
-  if err != nil {
-    app.notFoundResponse(w, r)
-    return
-  }
+	id, err := app.readIdParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
 
-  err = app.models.Movies.Delete(id)
-  if err != nil {
-    if errors.Is(err, data.ErrRecordNotFound) {
-      app.notFoundResponse(w, r)
-    } else {
-      app.serverErrorResponse(w, r, err)
-    }
-    return
-  }
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r)
+		} else {
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
 
-  err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
-  if err != nil {
-    app.serverErrorResponse(w, r, err)
-  }
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
-  var input struct{
-    Title string
-    Genres []string
-    Page int
-    PageSize int
-    Sort string
-  }
+	var input struct {
+		Title  string
+		Genres []string
+		data.Filters
+	}
 
-  v := validator.New()
-  qs := r.URL.Query()
+	v := validator.New()
+	qs := r.URL.Query()
 
-  input.Title = app.readString(qs, "title", "")
-  input.Genres = app.readCSV(qs, "title", []string{})
-  input.Page = app.readInt(qs, "page", 1, v)
-  input.PageSize = app.readInt(qs, "pagesize", 20, v)
-  input.Sort = app.readString(qs, "sort", "id")
+	input.Title = app.readString(qs, "title", "")
+	input.Genres = app.readCSV(qs, "title", []string{})
 
-  if !v.Valid() {
-    app.failedValidationResponse(w, r, v.Errors)
-    return
-  }
+	// data.Filters
+	input.Page = app.readInt(qs, "page", 1, v)
+	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Sort = app.readString(qs, "sort", "id")
 
-  fmt.Fprintf(w, "%+v", input)
+	data.ValidateFilters(v, data.Filters{
+		Page:         input.Page,
+		PageSize:     input.PageSize,
+		Sort:         input.Sort,
+		SortSafelist: []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"},
+	})
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", input)
 }
