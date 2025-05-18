60fe8e43a6906b7fcbb03595959b43fc51bf63d0
Travis Hopkins <trh.foss@protonmail.com>
2025-05-18 13:50:33 -0400

    fix: added per-IP rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
+
+	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -16,4 +21,52 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+  type client struct {
+    limiter *rate.Limiter
+    lastSeen time.Time
+  }
+
+  var (
+    mu sync.Mutex
+    clients = make(map[string]*client)
+  )
+
+  go func() {
+    for {
+      time.Sleep(time.Minute)
+
+      mu.Lock()
+
+      for ip, client := range clients {
+        if time.Since(client.lastSeen) > 3*time.Minute {
+          delete(clients, ip)
+        }
+      }
+
+      mu.Unlock()
+    }
+  }()
+
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    ip := realip.FromRequest(r)
+    mu.Lock()
+    if _, found := clients[ip]; !found {
+      clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+    }
+
+    clients[ip].lastSeen = time.Now()
+
+    if !clients[ip].limiter.Allow() {
+      mu.Unlock()
+      app.rateLimitExceededResponse(w, r)
+      return
+    }
+
+    mu.Unlock()
+
+    next.ServeHTTP(w, r)
+  })
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,13 @@
 module greenlight.camphopkins.com
 
-go 1.22.2
+go 1.23.0
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+toolchain go1.23.9
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
+	golang.org/x/time v0.11.0 // indirect
+)
 
 require github.com/lib/pq v1.10.9
