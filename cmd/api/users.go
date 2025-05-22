fb39388c33c3740368331ae848e106c0b8cb09e1
Travis Hopkins <trh.foss@protonmail.com>
2025-05-21 22:15:44 -0400

    fix: send email in goroutine after registerUserHandler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -53,7 +53,14 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
       return
     }
 
-    err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+    go func() {
+      err := app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+      if err != nil {
+        app.logger.Error(err.Error())
+      }
+    }()
+
+    err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
     if err != nil {
         app.serverErrorResponse(w, r, err)
     }
