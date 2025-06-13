f4bb19313c5f4d9bdf2a91e48b1a09e60207f086
Travis Reid Hopkins <trh.foss@protonmail.com>
2025-06-13 18:30:10 -0400

    fix: added citext to user table migration

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS movies (
   id bigserial PRIMARY KEY,
-  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW(),
   title text NOT NULL,
   year integer NOT NULL,
   runtime integer NOT NULL,
